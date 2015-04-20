bd4fe5ecd086b9477f36e0795d1550dbc261671f
mira <mirasaraswati.jtk12@gmail.com>
2015-04-20 17:49:54 +0700

    add example of login

---
+++ SistemPerpus/src/sistemperpus/model/Login.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemperpus.model;
+
+/**
+ *
+ * @author Mira
+ */
+public class Login {
+    private String username;
+    private String password;
+    public int attempt;
+    
+    public Login(String user, String passwd)
+    {
+        this.username = user;
+        this.password = passwd;
+        this.attempt = 0;
+    }
+    
+    public String getUsername() {
+        return this.username;
+    }
+    
+    public String getPassword() {
+        return this.username;
+    }
+    
+    public void setUsername (String user){
+        this.username=user;
+    }
+    
+    public void setPassword (String passwd){
+        this.password=passwd;
+    }
+    
+    public Boolean Login (String user, String passwd, int attemp)
+    {
+        if (attemp < 4)
+        {
+            if (user.equals(this.username) && (passwd.equals(this.password)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            attemp = attemp + 1;
+            return false;
+        }
+    }
+}
